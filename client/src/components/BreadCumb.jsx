599c56d1694f3f08d96488bafe158b6ce7d40557
wrefinity <wrefordmessi@gmail.com>
2024-03-04 23:29:15 +0100

    padding images

--- client/src/components/BreadCumb.jsx
+++ client/src/components/BreadCumb.jsx
@@ -1,12 +1,13 @@
 import React, { Fragment } from 'react';
 import johnx from '../assets/img/dash.png';
-import appx from '../assets/img/apple.png';
-import play from '../assets/img/play_black.png';
+import apple from '../assets/img/apple.png';
+import play from '../assets/img/play.png';
+
 
 const BreadCumb = () => {
   return (
     <Fragment>
-      <div className='bread'>
+      <div className='bread mb-5'>
         <div className='bread-left text-start nuller'>
           <h1>Welcome</h1>
           <h1>To 
@@ -14,16 +15,19 @@ const BreadCumb = () => {
           </h1>
           <p className='py-5'>Where Your Money Meets Innovation!</p>
           <div className='row d-flex mt-2 mx-0'>
-            <img
-              src={appx}
-              style={{ width: "100px", height: "40px", marginLeft:'0px' }}
-              alt='eleana'
-            />
-            <img
-              src={play}
-              style={{ width: "100px", height: "40px" }}
-              alt='eleana'
-            />
+          
+                            <img
+                                src={apple}
+                                style={{ width: "142px", height: "44px" }}
+                                className='mb-3'
+                                alt='eleana'
+                                />
+                            <img
+                                src={play}
+                                className='mb-3'
+                                style={{ width: "142px", height: "44px" }}
+                                alt='eleana'
+                            />
 
             <div className='row mt-5'>
               <div className=' col-md-4'>
