2296c6b417b2d8b128a14e66b5a0227bd56aba4e
wrefinity <wrefordmessi@gmail.com>
2024-06-11 22:30:40 +0100

    terms and condition

---
+++ client/src/components/Privacy copy.jsx
@@ -0,0 +1,127 @@
+import React from 'react'
+
+const Privacy = () => {
+  return (
+    <div className='privacy container py-5'>
+      <h1>Privacy <span>Policy</span></h1>
+
+      {/* <p className='txt-justify'> */}
+      <h2>Terms and Conditions</h2>
+      <p>These Terms and Conditions were last updated on April. 10th, 2024</p>
+      <p>These Terms and Conditions became effective on April. 10th, 2024</p>
+      <p>PAYSITA NIGERIA LIMITED, its parent, subsidiaries, successors, assignees and affiliates ("Paysita", "we", "our",
+        or "us") provide access to and use of paysita.com, a website owned and operated by Paysita, as well as any and all
+        other websites, mobile optimised sites, mobile applications, subdomains owned, operated or controlled by Paysita (the
+        "Website", "Paysita App"), together with the content, software, mobile services, financial products and functionality
+        offered on or through the Website and the Paysita App (collectively, the "Services").</p>
+      <p>These Terms and Conditions (the "Terms") as well as any and all specific terms and conditions for each of our
+        products and services, as amended from time to time, represent an agreement between you ("you", "your" or collectively
+        with other users, "Users") and Paysita and governs your use and access to our Services.</p>
+      <p>PLEASE READ THESE TERMS CAREFULLY TO ENSURE THAT YOU UNDERSTAND EACH PROVISION. BY GIVING YOUR OTP TO THE AGENT, OR
+        OTHERWISE USING THE SERVICES, WHETHER AS A GUEST OR REGISTERED USER, OR OTHERWISE ACCEPTING THESE TERMS, YOU ARE
+        SIGNIFYING THAT YOU HAVE READ AND UNDERSTOOD THE TERMS, AND AGREE TO BE BOUND BY THESE TERMS, OUR PRIVACY AND COOKIE
+        POLICY AND ALL FUTURE MODIFICATIONS TO THESE TERMS, AS WELL AS TO THE COLLECTION AND USE OF YOUR INFORMATION AS SET OUT
+        IN OUR PRIVACY AND COOKIE POLICY.</p>
+      <h2>1. DEFINITION</h2>
+      <p>1.1 “Paysita” means the mobile payments organization</p>
+      <p>1.2 “OTP” means one-time password (OTP), is a password that is valid for only one login session.</p>
+      <p>1.4 “Paysita Account” means an online account linked to the Paysita APP.</p>
+      <p>1.5 “Bill Payment” or “Pay Bill” means the service provided by Paysita including but not limited to the recharge
+        mobile bill payment, TV bill payment and electricity bill payment.</p>
+      <p>1.6 “Pay With Transfer” means a Virtual Account Number for money receiving service and is linked to the Paysita
+        Account.</p>
+      <p>1.7 “Virtual Account Number” means a personal ten-digit account number linked to the Paysita account for Pay With
+        Transfer service.</p>
+      <p>1.10 “Intellectual Property Rights” means all industrial and intellectual property rights of Paysita (as the context
+        provides), whether registered, or unregistered, including without limitation patents, trademarks and service marks,
+        designs and copyrights and any applications for any or in any part of the world, and the copyright in all drawings,
+        plans, specifications, designs and computer software owned by or licensed to Paysita (as the context provides) and used
+        in, or for the purposes of its business, and all Know-how and confidential information so owned and used.</p>
+      <h2>2. CONSENTING TO THESE TERMS</h2>
+      <p>By accessing or using the Services you consent to our Terms. In addition, you acknowledge that you have read,
+        understood and agreed to be bound by the terms and conditions outlined in these Terms and to comply with all our
+        applicable third-party service provider terms of use and agreements. Furthermore, you agree that our third-party
+        service providers are third-party beneficiaries of the applicable provisions of these Terms, with all rights to enforce
+        such provisions as if such Service providers were a party to these Terms.</p>
+      <h2>3. THE SERVICES WE PROVIDE</h2>
+      <h3>3.1 The Service of Paysita Account.</h3>
+      <p>3.1.1 For the User who registers a Paysita Account on the Paysita App, you can enjoy the services which include,
+        without limitation, the capability to: sign up as a customer; credit a Paysita account with cash through various
+        channels; pay or transfer money to a third party; transfer funds to a bank account; transfer funds to a Paysita
+        Account; pay Merchants; receive money from a third party; perform and/or complete certain functions relating to
+        Paysita’s Services.</p>
+      <h3>3.3 The Service of Pay With Transfer</h3>
+      <p>3.3.1 For the User who applies for Pay With Transfer service via the Paysita App, you will be randomly assigned a
+        Virtual Account Number which has linked to your Paysita Account.</p>
+      <p>3.3.2 You can use the bound Virtual Account Number to receive payments, accept transfers and other available
+        activities provided by PalmPay via PalmPay App. All transactions completed via Pay With Transfer service will be
+        reflected in your linked PalmPay Account balance.</p>
+      <p>3.4 All descriptions of proposed products and Services are based on assumptions (which are subject to change) and you
+        should not rely on the availability or functionality of products or Services until they are actually offered through
+        the Website or the Paysita App. We reserve the right in our sole discretion to determine how registration and other
+        promotions will be awarded. This determination includes, without limitation, the scope, nature and timing of all such
+        awards.</p>
+      <h2>4. THE CONDITIONS TO USE OUR SERVICES</h2>
+      <p>4.1 be at least 18 years of age to be able to access or use the Services or create a Paysita Account, as the Services
+        are not intended for children under 18. The Services are not available to any User previously removed from the
+        Services (or whose User Account has been terminated) by Paysita. You represent and warrant that you possess the legal
+        right and ability to enter into these Terms and to use the Services in accordance with these Terms;</p>
+      <p>4.2 have registered a Paysita Account on the Paysita App;</p>
+      <p>4.3 provide all information requested by us, such as your name, gender, date of birth, email address, passport
+        photograph, address, mobile phone number, Bank Verification Number (“BVN”), online credentials for registering with
+        PalmPay and such other information as we request from time to time (collectively, “User Information”); By giving Paysita
+        your BVN, you authorize Paysita to collect and save your data from the BVN database as part of our Know Your Customer
+        (“KYC”) information to fulfil regulatory requirements. All KYC data collected will be treated as confidential. You
+        also permit us to use your BVN to monitor, prevent and detect fraudulent activities and share the same with CBN
+        authorised stakeholders, for the purpose of deterring financial fraud; and</p>
+      <p>4.4 ensure that all User Information you provide to us from time to time is truthful, accurate, current and complete,
+        and you agree not to misrepresent your identity or your User Information. You agree to promptly notify us of changes
+        to your User information. If we approve of your registration, you will be authorized to use the Services, subject to
+        these Terms and provided that the Services will not be used for fraud.</p>
+      <p>4.5 promise and warrant that you only access and use our Services on your own. Paysita will not be responsible for any
+        third-party operation, unless with the written confirmation agreed by both parties.</p>
+      <h2>5. PROCEDURES INVOLVED IN USING THE SERVICES OF PAYSITA ACCOUNT</h2>
+      <h3>5.1 Adding Funding Sources to your Account</h3>
+      <p>5.1.1 You may access the available funds from other sources asides from your Account, you may link bank accounts and
+        cards to your Paysita Account.</p>
+      <p>5.1.2 Confirmation and authorization of your other funding sources by a one-time password, 3D Secure or other
+        authorization methods, as applicable are required. Once authorized, you grant Paysita and your bank the authority to
+        debit any of your source(s) at your request or at any reoccurring schedule you have indicated. Your authorization is
+        confirmed by your successful login to the Paysita platform (with the use of your password or fingerprint scan).</p>
+      <p>5.1.3 Once a funding source has been added to your Account, you grant Paysita the authority to save and store the
+        details of your funding source. Our Privacy and Cookie Policy will be applicable to all information stored and
+        processed.</p>
+      <p>5.1.4 Upon set up and authentication of the funding sources added to your Account, all payments processed from your
+        Account are deemed genuine and Paysita shall not be held liable for any form of fraud, oversight or compromise.</p>
+      <p>5.1.5 You will be responsible for any charges or penalties from third parties or your bank that occurs as a result of
+        any transactions carried out on your Account.</p>
+      <p>5.1.6 Do note that some transactions may carry a fee. Our Paysita fee schedules are available on our website, through
+        communication with our Customer Services and will be displayed on the Paysita App before any transaction is
+        confirmed. The fee schedule provides details on the fee amounts, and the method and source of payment for such fees.
+        Paysita reserves the sole discretion to revise the fee schedule from time to time.</p>
+      <p>5.1.7 You will be responsible for managing your funding sources, in terms of additions, removals and updates e.g.
+        expired debit cards.</p>
+      <h3>5.2 Transacting</h3>
+      <p>5.2.1 You may access the available funds in your Account at any time using any of the channels you choose, including
+        your mobile. Whilst we make every reasonable attempt to provide the services as described in our marketing and
+        educational materials, we offer the services “as is” and without any warranties. Transactions include but are not
+        limited to the following: funds transfer, cash-in, cash-out, airtime purchase, bills payment and merchant payments.</p>
+      <p>5.2.2 All payments will be processed in Nigerian local currency i.e. Naira and Kobo.</p>
+      <p>5.2.3 Each transaction will be identified by a unique Transaction ID, which is used to track and identify all
+        transactions carried out on the Paysita network. Your Transaction ID is important for a variety of uses, including
+        settlement of any issues arising from transactions carried out on your account.</p>
+      <p>5.2.4 Paysita will send you a receipt of all transactions effected from your Paysita Account or performed as an Agent; this message may be sent via smartphone notification, SMS, email, or another available channel. </p>
+      <p>
+      5.2.5 Your Account will be credited with any deposit made into it, and the funds will be available for your use once the transaction has been approved. 
+      </p>
+
+      <h2> PROCEDURES INVOLVED IN USING THE SERVICES OF PAY WITH TRANSFER </h2>
+      <p> 6.1 You can apply for Pay With Transfer service- </p>
+      <p>6.1.1 via the Paysita App application page. You will be randomly assigned a Virtual Account Number which has linked to your Paysita Account; or/and</p>
+      <p>6.2 Any marketing activity of Pay With Transfer service is subject to the page display in Paysita App. </p>
+      <p> 6.3 Paysita reserve the right to temporarily suspend your Pay With Transfer service at any time if we believe there is a risk. </p>
+    </div>
+  )
+}
+
+export default Privacy
